b3c6b59a035b26219d479b619ba940c34706648a
Vinay Yadav <ydavvinay@gmail.com>
2026-09-11 15:20:55 +0530

    Add static product image

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const ProductDetails = async ({
   params,
 }: {
@@ -10,9 +12,13 @@ const ProductDetails = async ({
   });
 
   const product = await res.json();
+
   return (
     <main>
       <h1>{product.title}</h1>
+
+      <Image src="/product.jpg" alt={product.title} width={300} height={300} />
+
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
     </main>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,18 +2,23 @@ import Link from "next/link";
 
 const Products = async () => {
   const res = await fetch("https://dummyjson.com/products", {
-    next: { revalidate: 60 },
+    next: { revalidate: 50 },
   });
+
   const data = await res.json();
 
   return (
-    <ul>
-      {data.products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <main>
+      <h1>Products Page</h1>
+
+      <ul>
+        {data.products.map((product) => (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 };
 
